f9033d29cc63a393153189ace8dd2a01bbd9d91d
JJPeng <pengjiajun755@163.com>
2022-07-23 20:40:26 +0800

    通过oauth2进行认证授权之后，token信息被保存在Security Context中 增加日志输出来验证

--- src/main/java/com/jjpeng/oauth2client/controller/MainController.java
+++ src/main/java/com/jjpeng/oauth2client/controller/MainController.java
@@ -1,7 +1,10 @@
 package com.jjpeng.oauth2client.controller;
 
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.logging.Logger;
 
 /**
  * @author JJPeng
@@ -10,8 +13,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
+    private Logger logger = Logger.getLogger(MainController.class.getName());
+
     @GetMapping("/")
-    public String main() {
+    public String main(OAuth2AuthenticationToken token) {
+        //认证之后，认证过滤器将用户的信息存储到Security Context中
+        logger.info(String.valueOf(token.getPrincipal()));
         return "main.html";
     }
 }
